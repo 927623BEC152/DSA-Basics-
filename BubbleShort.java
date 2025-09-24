bf52b9e580a4492024e64a6726925a67394e725f
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-24 14:30:29 +0530

    logic change and file rename

--- BubbleShort.java
+++ BubbleShort.java
@@ -9,8 +9,8 @@ public class BubbleShort {
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i-1;j++){
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp=arr[j];
                     arr[j]=arr[j+1];
