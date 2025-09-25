b211bb41aa8ae4c453d3fdd90d3ef39fcfbf07f2
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-25 15:51:22 +0530

    using LinkedList

--- StackBuiltin.java
+++ StackBuiltin.java
@@ -1,7 +1,7 @@
-import java.util.Stack;
+import java.util.LinkedList;
 public class StackBuiltin {
     public static void main(String[] args){
-        Stack <Integer> s = new Stack <> ();
+        LinkedList <Integer> s = new LinkedList <> ();
         s.push(10);
         s.push(20);
         s.push(30);
