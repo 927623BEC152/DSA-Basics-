ac9f65afe7d2eeb6c38ed49e2a6740fc3d190a5c
927623BEC152 <927623BEC152@mkce.ac.in>
2025-09-26 12:00:43 +0530

    program inserted

--- StackProblemBuilt.java
+++ StackProblemBuilt.java
@@ -1,5 +1,26 @@
+import java.util.*;
 import java.util.Stack;
 public class StackProblemBuilt {
-    
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int type = sc.nextInt();
+            if (type == 1) {      
+                int x = sc.nextInt();
+                s.push(x);
+            } else if (type == 2) { 
+                if (!s.isEmpty()) {
+                    System.out.println(s.pop());
+                } else {
+                    System.out.println(-1); 
+                }
+            }
+        }
+        sc.close();
+    }
 }
+
